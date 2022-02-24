7b1dec391d7c812b1a65c19b1b898817a49c16cc
ankurkashyap <ankurkashyap290@gmail.com>
2022-02-24 18:00:08 +0530

    page pushed

--- src/redux/investor/reducer.js
+++ src/redux/investor/reducer.js
@@ -110,17 +110,17 @@ const investorReducer = (state = initState, action) => {
 				error: null,
 			};
 		case GET_ALL_INVESTORS_SUCCESS:
-			// const records = {
-			// 	...action.response,
-			// 	results: state.investors
-			// 		? [...state.investors.results, ...action.response.results]
-			// 		: action.response.results,
-			// };
+			const records = {
+				...action.response,
+				results: state.investors
+					? [...state.investors.results, ...action.response.results]
+					: action.response.results,
+			};
 			return {
 				...state,
 				loading: false,
 				isMobileVerified: true,
-				investors: action.response,
+				investors: records,
 			};
 		case GET_ALL_INVESTORS_ERROR:
 			return {
