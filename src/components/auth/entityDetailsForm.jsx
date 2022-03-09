6592a0da2dc991a4c214aa3d1bd4dc149ed814d8
Shallu Thakur <shallu@webmobsoft.com>
2022-03-09 13:26:45 +0530

    new-changes 9 march

--- src/components/auth/entityDetailsForm.jsx
+++ src/components/auth/entityDetailsForm.jsx
@@ -500,7 +500,8 @@ const EntityDetailsForm = ({ setActiveTab, setEntityDetails }) => {
 									)}
 								</Form.Group>
 							</Row>
-							<Button className="btn-position btn-filled w-custom" type="submit">
+							<div className="btn-register">
+							<Button className="btn-filled w-custom" type="submit">
 								Next Step{" "}
 								<img
 									src={"/assets/images/arrowblue.png"}
@@ -508,6 +509,7 @@ const EntityDetailsForm = ({ setActiveTab, setEntityDetails }) => {
 									className="icon-login-left"
 								/>
 							</Button>
+							</div>
 						</Form>
 					);
 				}}
