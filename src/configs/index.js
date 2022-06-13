cc1060a1d2d062a5865d4460d9a1f488a2bed671
ankurkashyap <ankurkashyap290@gmail.com>
2022-06-13 17:26:15 +0530

    staging

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://uat.dltkash.com/api/v1"; //"https://tender-frost-20356.pktriot.net/api/v1"; //"http://uat.dltkash.com/api/v1"; //"https://silent-paper-45295.pktriot.net/api/v1"; //"http://uat.dltkash.com/api/v1";
+export const API_URL = "https://staging.dltkash.com/api/v1"; //"https://tender-frost-20356.pktriot.net/api/v1"; //"http://uat.dltkash.com/api/v1"; //"https://silent-paper-45295.pktriot.net/api/v1"; //"http://uat.dltkash.com/api/v1";
 export const USER_TOKEN_KEY = "----dltkashUserTokenKey@2022----";
 export const USER_KEY = "----dltkashUserKey@2022----";
