ace327c05008af44f7e61e4c157fa8fa0a94a30b
ankurkashyap <ankurkashyap290@gmail.com>
2022-06-13 13:41:16 +0530

    fixes webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,12 @@ module.exports = {
 	entry: path.join(__dirname, "src", "index.js"),
 	output: {
 		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	devServer: {
+		port: 3000,
+		contentBase: "./",
+		hot: true,
 	},
 	module: {
 		rules: [
